deb7cde027ccea9c3cbb6e34e9afc1fd72e23d4c
Amit Upadhyay <amitu@browserstack.com>
2015-09-16 18:18:06 +0530

    started

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+try:
+    long_description = open('README.md', 'rt').read()
+except Exception:
+    long_description = ""
+
+setup(
+    name="amdfix",
+    description="amdfix fixes AMD files",
+    long_description=long_description,
+
+    version="0.0.1",
+    author='Amit Upadhyay',
+    author_email="upadhyay@gmail.com",
+
+    url='https://github.com/amitu/amdfix',
+    license='BSD',
+
+    install_requires=[
+    ],
+    packages=["amdfix"],
+
+    zip_safe=True,
+)
